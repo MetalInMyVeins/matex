d924db3284e0dbc691b42b2dc40e30c484e5c05b
Hossain Md. Fahim <hossainmdfahim66@gmail.com>
2026-01-14 12:02:03 +0600

    docs: Add feature comments

--- src/main.cxx
+++ src/main.cxx
@@ -4,10 +4,16 @@
 int main(int argc, char** argv)
 {
   std::vector<std::string> cmd_args{};
+  cmd_args.reserve(static_cast<size_t>(argc));
   for (int i{1}; i < argc; ++i)
   {
     cmd_args.push_back(*(argv + i));
   }
+
+  // Matex matex;
+  // matex.compile("3 + 2 - 9");
+  // matex.getAsm();
+
   return 0;
 }
 
